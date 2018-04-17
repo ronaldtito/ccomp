7e15f4733aa29ae272d72887f8ce6ff5e42b33c0
ronaldtito <ronaldtito@users.noreply.github.com>
2018-04-17 13:21:02 -0500

    Update lista_doble_puntero.cpp

--- lista_doble_puntero.cpp
+++ lista_doble_puntero.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+using namespace std;
 
 
 template <class T>
@@ -29,7 +29,16 @@ class Node
 public:
     T data;
     Node * next;
+    Node();
+    Node(T x);
 };
+
+template <class T>
+Node<T>::Node(){ next = NULL;}
+
+template <class T>
+Node<T>::Node(T x){ data = x ;next = NULL;}
+
 
 template <class T , class Op>
 class Clist
@@ -40,18 +49,34 @@ public:
     bool Insert(T x);
     bool Remove(T x);
     void Print();
+    Clist();
+    ~Clist();
 };
+
+template <class T, class Op>
+Clist<T,Op>::Clist(){ head = NULL;}
+
+template <class T, class Op>
+Clist<T, Op>::~Clist()
+{
+    Node<T> **temp = &head, **prev;
+    while(( *temp )!= NULL )
+    {
+        (*prev)=(*temp)->next;
+        delete *temp;
+        temp=prev;
+    }
+}
 
 template <class T , class Op>
 bool Clist<T,Op>::Find(T x, Node<T> **&p)
 {
     Op op;
     for(p= &head; *p != NULL && op((*p)->data, x) ; p = &(*p)->next);
-    /*if((*p) != NULL && (*p)->data == x )
+    if((*p) != NULL && (*p)->data == x )
         return true;
     else
-        return false;*/
-    return (*p) && (*p)->data == x;
+        return false;
 }
 
 template <class T , class Op>
@@ -81,8 +106,29 @@ bool Clist<T,Op>::Remove(T x)
     return false;
 }
 
-int main() {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+template <class T, class Op>
+void Clist<T, Op>::Print()
+{
+    Node<T> *temp=head;
+    while(temp != NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    Clist<int, c_less<int> > *Lista1 = new Clist<int, c_less<int> >();
+    Lista1->Insert(5);
+    Lista1->Insert(3);
+    Lista1->Insert(2);
+    Lista1->Insert(10);
+    Lista1->Insert(4);
+    Lista1->Remove(3);
+    Lista1->Remove(2);
+    Lista1->Print();
+    //delete Lista1;
+    
 }
